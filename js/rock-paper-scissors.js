f00bcc326eeecba7e5ee2df698a6a7c2e21c82dd
gurpsehm <gurpreetsehmbey1@gmail.com>
2022-03-16 12:29:09 -0600

    Added function for the computer's move and also tested it to make sure it displays the correct message

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+function computerPlay () {
+  const moves = Array("rock", "paper", "scissors");
+  const move = moves[Math.floor(Math.random() * moves.length)];
+  return move.toString();
+}
+
+console.log(computerPlay());
